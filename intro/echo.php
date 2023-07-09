4c38364491208c7f28a6af0c2157ce0d09708924
Diego Reis <diegoreso@me.com>
2023-07-09 16:33:08 -0300

    inicio curso

---
+++ intro/echo.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Aula2</title>
+</head>
+
+<body>
+  <h1>Testando php</h1>
+
+  <?php
+  
+  echo "Hello World PHP"
+  
+  
+  ?>
+</body>
+
+</html>
---
+++ intro/exe1.php
@@ -0,0 +1,7 @@
+<?php 
+  
+$nome = "Diego";
+
+  echo $nome;
+  phpinfo()
+?>
